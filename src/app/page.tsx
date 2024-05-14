d39e7dcf9e856703bef10d639fe746c884d8be01
giuraionut <giuraionut98@gmail.com>
2024-05-14 10:17:56 +0300

    code refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Tag, Card, Modal, Flex } from 'antd';
-import { DateLogicComponent } from './components/DateLogicComponent';
-import { Moods } from './components/Moods';
 import { DayMood, Mood } from '@prisma/client';
+import { HeartOutlined } from '@ant-design/icons';
 
 interface IDay {
   title: string;
@@ -13,29 +12,121 @@ interface IDay {
 }
 
 const Home: React.FC = () => {
-  const {
-    selectedMonth,
-    setSelectedMonth,
-    currentYear,
-    selectedDay,
-    currentDay,
-    setSelectedDay,
-    getAllDaysOfMonth,
-    allMonths,
-  } = DateLogicComponent();
+  interface IMonth {
+    title: string;
+    icon: JSX.Element; // Updated 'icon' type to JSX.Element
+    color: string;
+    index: number;
+    currentMonth?: boolean;
+  }
 
-  const moods = Moods();
+  interface MyDayMood extends DayMood {
+    mood: Mood;
+  }
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [days, setDays] = useState<IDay[]>([]);
 
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth(); // Note: Months are zero-based (0 = January, 1 = February, etc.)
+  const currentDay = currentDate.getDate();
+  const [selectedDay, setSelectedDay] = useState(currentDay);
+
   const [loading, setLoading] = useState(true);
+  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
+
+  const allMonths: IMonth[] = [
+    { title: 'January', icon: <HeartOutlined />, color: 'blue', index: 0 },
+    { title: 'February', icon: <HeartOutlined />, color: 'red', index: 1 },
+    { title: 'March', icon: <HeartOutlined />, color: 'green', index: 2 },
+    { title: 'April', icon: <HeartOutlined />, color: 'red', index: 3 },
+    { title: 'May', icon: <HeartOutlined />, color: 'green', index: 4 },
+    { title: 'June', icon: <HeartOutlined />, color: 'red', index: 5 },
+    { title: 'July', icon: <HeartOutlined />, color: 'blue', index: 6 },
+    { title: 'August', icon: <HeartOutlined />, color: 'cyan', index: 7 },
+    { title: 'September', icon: <HeartOutlined />, color: 'blue', index: 8 },
+    { title: 'October', icon: <HeartOutlined />, color: 'orange', index: 9 },
+    { title: 'November', icon: <HeartOutlined />, color: 'blue', index: 10 },
+    { title: 'December', icon: <HeartOutlined />, color: 'green', index: 11 },
+  ].map((month) => ({
+    ...month,
+    currentMonth: month.index === currentMonth,
+  }));
+  const daysInMonth = (month: number, year: number) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+
+  const [moods, setMoods] = useState<Mood[]>([]); // Annotate moods as Mood[]
+
   useEffect(() => {
-    console.log('USE EFFECT');
-    console.log('selectedMonth', selectedMonth);
-    const updatedDays = getAllDaysOfMonth(selectedMonth, currentYear);
-    setDays(updatedDays);
-    console.log(updatedDays);
-    setLoading(false);
+    const fetchMoods = async () => {
+      try {
+        const response = await fetch('/api/mood', {
+          headers: {
+            Accept: 'application/json',
+            method: 'GET',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+
+          setMoods(data);
+        } else {
+          throw new Error('Failed to fetch moods');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchMoods();
+  }, []);
+
+  useEffect(() => {
+    console.log('useeffect');
+    const fetchDayMoods = async () => {
+      try {
+        const response = await fetch('/api/daymood', {
+          headers: {
+            Accept: 'application/json',
+            method: 'GET',
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+
+          const totalDays = daysInMonth(selectedMonth, currentYear);
+          const d = Array.from({ length: totalDays }, (_, index) => {
+            const date = new Date(currentYear, selectedMonth, index + 1);
+            const title = date.toLocaleString('en-US', { weekday: 'long' });
+            const moodForDay = data.find(
+              (daymood: DayMood) =>
+                daymood.day === index + 1 &&
+                daymood.month === selectedMonth &&
+                daymood.year === currentYear
+            );
+            return {
+              index: index + 1,
+              title,
+              currentDay:
+                index + 1 === currentDay && selectedMonth === currentMonth,
+              mood: moodForDay?.mood,
+            };
+          });
+          setDays(d);
+          setLoading(false); // Move setLoading inside the success branch of the if statement
+          console.log('data', data);
+        } else {
+          throw new Error('Failed to fetch day moods');
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false); // Move setLoading inside the catch block to handle errors
+      }
+    };
+
+    fetchDayMoods();
   }, [selectedMonth, currentYear]);
 
   const showModal = () => setIsModalOpen(true);
@@ -99,14 +190,15 @@ const Home: React.FC = () => {
   const daysCards = days
     ? days.map((day) => (
         <Card
-          key={day.index}
-          className={`${
+          style={
             day.mood
-              ? `bg-${day.mood.color}-700`
+              ? { backgroundColor: day.mood.color }
               : day.currentDay
-              ? 'bg-blue-400'
-              : 'bg-slate-700'
-          } text-white text-center border-none hover:cursor-pointer hover:brightness-125`}
+              ? { backgroundColor: ' rgb(96, 165, 250)' }
+              : { backgroundColor: 'rgb(51 65 85)' }
+          }
+          key={day.index}
+          className='text-white text-center border-none hover:cursor-pointer hover:brightness-125'
           onClick={() => {
             setSelectedDay(day.index);
             showModal();
@@ -116,6 +208,26 @@ const Home: React.FC = () => {
         </Card>
       ))
     : [];
+  // const daysCards = days
+  //   ? days.map((day) => (
+  //       <Card
+  //         key={day.index}
+  //         className={`${
+  //           day.mood
+  //             ? `bg-${day.mood.color}-700`
+  //             : day.currentDay
+  //             ? 'bg-blue-400'
+  //             : 'bg-slate-700'
+  //         } text-white text-center border-none hover:cursor-pointer hover:brightness-125`}
+  //         onClick={() => {
+  //           setSelectedDay(day.index);
+  //           showModal();
+  //         }}
+  //       >
+  //         {day.index} - {day.title}
+  //       </Card>
+  //     ))
+  //   : [];
 
   return loading ? (
     'loading...'
@@ -131,7 +243,7 @@ const Home: React.FC = () => {
           {moodTags}
         </Flex>
       </Modal>
-      
+
       <div className='bg-slate-500 h-screen'>
         <div className='grid grid-cols-2 gap-2 p-5'>
           <div className='flex flex-col gap-3 p-5'>
